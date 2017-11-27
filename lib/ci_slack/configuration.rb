5dbb85e138574418c3b314d2403f1f15930304bc
skrinits <110100reg@gmail.com>
2017-11-27 07:52:33 +0300

    replaced a module

--- lib/ci_slack/configuration.rb
+++ lib/ci_slack/configuration.rb
@@ -1,14 +1,16 @@
-class CiSlack::Configuration
-  attr_accessor :icon, :webhook, :channel, :ci_computer,
-                :bot_name, :project, :slack_names
+module CiSlack
+  class Configuration
+    attr_accessor :icon, :webhook, :channel, :ci_computer,
+                  :bot_name, :project, :slack_names
 
-  def initialize
-    @icon = 'failed'
-    @webhook = ''
-    @channel = '#ci'
-    @bot_name = 'CI BOT'
-    @project = ''
-    @slack_names = {}
-    @ci_computer = 'CI'
+    def initialize
+      @icon = 'failed'
+      @webhook = ''
+      @channel = '#ci'
+      @bot_name = 'CI BOT'
+      @project = ''
+      @slack_names = {}
+      @ci_computer = 'CI'
+    end
   end
 end
--- lib/ci_slack/messager.rb
+++ lib/ci_slack/messager.rb
@@ -1,36 +1,38 @@
 require 'slack-notifier'
 require 'ci_slack'
 
-class CiSlack::Messager
-  attr_reader :client, :project
+module CiSlack
+  class Messager
+    attr_reader :client, :project
 
-  def initialize
-    @client = ::Slack::Notifier.new(webhook, channel: channel, username: bot_name)
-  end
-
-  def send(message)
-    return unless ENV[ci_computer.to_s]
-
-    author, commit_message = last_git_log
-    slack_name = slack_names.select { |key, _| author.downcase =~ key }.values.first || author
-
-    text = "#{ project }. CI FAILED!\n<@#{ slack_name }> : #{ commit_message } \n#{ message }"
-
-    client.post(text: text, icon_emoji: ":#{ icon }:")
-  end
-
-  def method_missing(method, *args)
-    if %i[icon webhook channel ci_computer
-          bot_name project slack_names].include?(method)
-      CiSlack.configuration.send(method)
-    else
-      super
+    def initialize
+      @client = ::Slack::Notifier.new(webhook, channel: channel, username: bot_name)
     end
-  end
 
-  private
+    def send(message)
+      return unless ENV[ci_computer.to_s]
 
-  def last_git_log
-    `git log -1 --pretty='%an||%s'`.split('||').map(&:strip)
+      author, commit_message = last_git_log
+      slack_name = slack_names.select { |key, _| author.downcase =~ key }.values.first || author
+
+      text = "#{ project }. CI FAILED!\n<@#{ slack_name }> : #{ commit_message } \n#{ message }"
+
+      client.post(text: text, icon_emoji: ":#{ icon }:")
+    end
+
+    def method_missing(method, *args)
+      if %i[icon webhook channel ci_computer
+            bot_name project slack_names].include?(method)
+        CiSlack.configuration.send(method)
+      else
+        super
+      end
+    end
+
+    private
+
+    def last_git_log
+      `git log -1 --pretty='%an||%s'`.split('||').map(&:strip)
+    end
   end
 end
